d1f320f52c85a791da49010835edb1e4376ddccb
Spyne AI <spyneai@Gouravs-M1.local>
2022-02-25 13:40:53 +0530

    first commit

---
+++ spynesdkapplication/src/main/java/com/spyneai/spynesdkapplication/MainSDKActivity.kt
@@ -0,0 +1,14 @@
+package com.spyneai.spynesdkapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSDKActivity : AppCompatActivity() {
+
+    var hiSDK = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
